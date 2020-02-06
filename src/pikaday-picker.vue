20b05a6c6474f3d9f75fd7e2afeee180ceb589e4
Kamal <kamalalbatarny@gmail.com>
2020-02-05 21:49:38 -0300

    first commit

---
+++ src/pikaday-picker.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="text" ref="input" :value="value" />
+</template>
+
+<script>
+import Pikaday from "pikaday";
+import "pikaday/css/pikaday.css";
+
+export default {
+  props: {
+    value: {
+      required: true
+    },
+    format: {
+      default: "YYYY-MM-DD"
+    },
+    options: {
+      default: () => ({})
+    }
+  },
+  mounted: function() {
+    const picker = new Pikaday({
+      field: this.$refs.input,
+      format: this.format,
+      onSelect: () => {
+        this.$emit("input", picker.toString());
+      },
+      ...this.options
+    });
+  }
+};
+</script>
---
+++ src/serve-dev.vue
@@ -0,0 +1,28 @@
+<script>
+import Vue from "vue";
+import PikadayPicker from "@/pikaday-picker.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    PikadayPicker
+  },
+  data() {
+    return {
+      defaultDate: "19/11/1990",
+      date: ""
+    };
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <pikaday-picker
+      v-model="date"
+      value="19/11/1990"
+      format="DD/MM/YYYY"
+      :options="{ firstDay: 3 }"
+    />
+  </div>
+</template>
